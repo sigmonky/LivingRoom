55af68e4e0f7ce88d14bc0330c2214cbf604a385
Isaac Silva <isaacsou@gmail.com>
2011-06-28 11:38:02 -0400

    deploy

--- app/app-variables.js
+++ app/app-variables.js
@@ -94,24 +94,6 @@ function getCookie(c_name)
 	}
 }
 
-App.Util = {
-	
-	facebookAccessToken: '',
-	
-	setFacebookTokenFromUrl: function(){
-		queryObj = {};
-        var qs = window.location.href;
-		var fragments = qs.split('#');
-		//console.log('fragments  '+fragments);
-		if (fragments.length >= 2) {
-			var final = fragments[1].indexOf('&expires_in=')
-            qs = fragments[1].substring(13, final);
-			this.facebookAccessToken = qs;
-        }else{
-			this.facebookAccessToken = "";
-		}
-	}
-}
 
 function delCookie(name) {
 	document.cookie = name + '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
