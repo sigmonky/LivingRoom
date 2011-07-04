c07830abe107ecfd20cc679dde926ac8768b5611
Logic Diner <logicdiner@gmail.com>
2011-07-04 15:52:01 -0700

    muc

--- app/stores/FacebookUser.js
+++ app/stores/FacebookUser.js
@@ -6,7 +6,7 @@
  * @class LivingRoomAPI.stores.OnlineUsers
  * Definition of the store able to contains all the Online users.
  */
-Ext.regStore('OnlineUsers', {
+Ext.regStore('FacebookUser', {
 	model: 'FacebookUser',
 	autoLoad: true,
 	proxy: {
