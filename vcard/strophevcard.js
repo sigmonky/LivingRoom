c550badb93c4207c86515ee43291cd2b60af8ab7
Logic Diner <logicdiner@gmail.com>
2011-07-04 20:07:11 -0700

    muc

--- vcard/strophevcard.js
+++ vcard/strophevcard.js
@@ -1,13 +1,13 @@
 var StropheVcard = function(window, $) {
     var BOSH_URL = "/http-bind";
-    var DOMAIN = "http://www.logoslogic.com";
+    var DOMAIN = "logoslogic.com";
     var con = null;
     var _connected = false;
     var onResult = function(stanza) {
     };
     var obj = {
         init: function() {
-            conn = new Strophe.Connection(BOSH_URL);            
+            conn = new Strophe.Connection("http://www.logoslogic.com/http-bind");            
         },
         connected: function() {
             return _connected;
@@ -22,7 +22,9 @@ var StropheVcard = function(window, $) {
         },
 
         connect: function(handler) {
-            conn.connect(DOMAIN, null, function (status) {
+			console.log('connect');
+	
+            conn.connect('isaacueca@logoslogic.com','cigano', function (status) {
                 if (status === Strophe.Status.CONNECTED) {
 					console.log('connected')
                     _connected = true;
@@ -36,9 +38,20 @@ var StropheVcard = function(window, $) {
 
 $(document).ready(function(){
 	console.log('ready');
+/*	var conn = new Strophe.Connection("http://www.logoslogic.com/http-bind");
+	
+	conn.connect(data.jid, data.password, function (status) {
+	        if (status === Strophe.Status.CONNECTED) {
+				console.log('CONNECTED')
+	        } else if (status === Strophe.Status.DISCONNECTED) {
+				console.log('DISCONNECTED')
+	        }
+	 });*/
+	
     StropheVcard.init();
     StropheVcard.connect(function() {
         StropheVcard.get(function(stanza) {
+		console.log('handler');
             var $vCard = $(stanza).find("vCard");
             var img = $vCard.find('BINVAL').text();
             var type = $vCard.find('TYPE').text();
