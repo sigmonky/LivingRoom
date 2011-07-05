1715d03eefa5106728ad2706899b3cd01646d58d
Logic Diner <logicdiner@gmail.com>
2011-07-04 18:14:55 -0700

    muc

--- register/register2.php
+++ register/register2.php
@@ -55,7 +55,7 @@ $this->jab->NewUserName = $name;
 $this->jab->NewUserPass = $pass;
 $this->GivenName = $firstn;
 $this->FamilyName = $lastn;
-$this->MiddleName = $patro);
+$this->MiddleName = $patro;
 }
 
 function handleConnected()
@@ -77,12 +77,158 @@ $this->jab->addvcard_request($this->GivenName, $this->FamilyName, $this->MiddleN
 /******************************************************************************************************/
 
 // Including original "Jabber Client Library" - class
+require_once(dirname(__FILE__).'jabber.php');
 
 /******************************************************************************************************/
 
 // This is extension to basic Jabber class
 
+class CommandJabber extends Jabber
+{
+var $AddUserDialogID=0;
+var $NewUserName, $NewUserPass;
 
+function adduser_init()
+{
+$this->AddUserDialogID = $this->_unique_id('adduserproc');
 
+$this->_set_iq_handler('_on_adduser_initanswer',$this->AddUserDialogID);
+
+$xml = '<iq from="'.($this->jid).'" id="'.$this->AddUserDialogID.'" to="'.($this->_server_host).'" type="set">
+<command xmlns="http://jabber.org/protocol/commands" action="execute" node="http://jabber.org/protocol/admin#add-user"/>
+</iq>';
+return $this->_send($xml);
+}
+
+function _on_adduser_initanswer(&$packet)
+{
+global $AddUserErrorCode;
+$AddUserErrorCode=12004;
+if ($this->_node($packet,array('iq','@','type'))=='result') // if isn't an error response
+{
+$AddUserErrorCode=12005;
+$sessionid=$this->_node($packet,array('iq','#','command','0','@','sessionid'));
+if (strlen($sessionid) && $this->_node($packet,array('iq','#','command','0','@','status'))=='executing') // response seems to be OK
+  {
+  $AddUserErrorCode=12006;
+  $xml='<iq from="'.($this->jid).'" id="'.$this->AddUserDialogID.'" to="'.($this->_server_host).'" type="set"><command xmlns="http://jabber.org/protocol/commands" node="http://jabber.org/protocol/admin#add-user" sessionid="'.$sessionid.'"><x xmlns="jabber:x:data" type="submit">';
+  $fieldsnode=$this->_node($packet,array('iq','#','command','0','#','x','0','#','field'));
+  $i=0;
+  do
+    {
+$field_type=$this->_node($fieldsnode,array($i,'@','type'));
+$field_var=$this->_node($fieldsnode,array($i,'@','var'));
+$field_value=$this->_node($fieldsnode,array($i,'#','value','0','#'));
+
+if ($field_type=='hidden') $xml.='<field type="hidden" var="'.$field_var.'"><value>'.$field_value.'</value></field>';
+if ($field_var=='accountjid') $xml.='<field type="'.$field_type.'" var="accountjid"><value>'.$this->NewUserName.'@'.$this->_server_host.'</value></field>';
+if ($field_var=='password') $xml.='<field type="'.$field_type.'" var="password"><value>'.$this->NewUserPass.'</value></field>';
+if ($field_var=='password-verify') $xml.='<field type="'.$field_type.'" var="password-verify"><value>'.$this->NewUserPass.'</value></field>';
+$i++;
+}
+  while (strlen(trim($field_type)) && $i<20);
+ 
+  $xml.='</x></command></iq>';
+  $this->_set_iq_handler('_on_adduser_getresult',$this->AddUserDialogID);
+  $this->_send($xml);
+  }
+}
+}
+
+function _on_adduser_getresult(&$packet)
+{
+global $AddUserErrorCode;
+$AddUserErrorCode=12007;
+if ($this->_node($packet,array('iq','@','type'))=='result')
+{
+if ($this->_node($packet,array('iq','#','command','0','@','status'))=='completed');
+$AddUserErrorCode=0;
+}
+
+$this->terminated = true;
+}
+
+// following functions - for fill Vcard only
+
+function addvcard_request($GivenName, $FamilyName, $MiddleName)
+{
+$DialogID = $this->_unique_id('addvcard');
+
+$this->_set_iq_handler('_on_addvcard_reply',$DialogID);
+
+$xml = '<iq from="'.($this->jid).'" id="'.$DialogID.'" type="set">
+<vCard xmlns="vcard-temp">
+<N><FAMILY>'.$FamilyName.'</FAMILY><GIVEN>'.$GivenName.'</GIVEN><MIDDLE>'.$MiddleName.'</MIDDLE></N>
+</vCard>
+</iq>';
+return $this->_send($xml);
+}
+
+function _on_addvcard_reply(&$packet)
+{
+global $AddVcardErrorCode;
+$AddVcardErrorCode=14004;
+
+if ($this->_node($packet,array('iq','@','type'))=='result') $AddVcardErrorCode=0;
+
+$this->terminated = true;
+}
+
+} // End of Jabber class extension
+
+/******************************************************************************************************/
+// NOW WE START TO USE ALL CLASSES ABOVE :)
+
+// create an instance of the Jabber class
+$display_debug_info = false;
+$AddUserErrorCode = 12000;
+$UserLogin='isaacueca'; 
+$UserPass='cigano';
+$FirstName='Philip'; 
+$LastName='J.'; 
+$Patronymic='Ivanovich :)';
+
+$jab = new CommandJabber($display_debug_info);
+
+$addmsg = new AddMessenger($jab,$UserLogin,$UserPass);
+
+// set handlers for the events we wish to be notified about
+$jab->set_handler("connected",$addmsg,"handleConnected");
+$jab->set_handler("authenticated",$addmsg,"handleAuthenticated");
+//$jab->set_handler("error",$addmsg,"handleError");
+
+// connect to the Jabber server
+if ($jab->connect(JABBER_SERVER))
+{
+	$AddUserErrorCode=12001;
+	$jab->execute(CBK_FREQ,RUN_TIME);
+}
+
+$jab->disconnect();
+
+unset($jab,$addmsg);
+
+echo '<P>******** Exit of User Creation! ErrorCode='.$AddUserErrorCode.' ********</P>';
+
+// If AddUserErrorCode is 0, we can try to fill user's Vcard, using brand new credentials :)
+
+$AddVcardErrorCode = 14000;
+$jab = new CommandJabber($display_debug_info);
+$avcard = new AddVcard($jab,$UserLogin,$UserPass,$FirstName,$LastName,$Patronymic);
+
+$jab->set_handler("connected",$avcard,"handleConnected");
+$jab->set_handler("authenticated",$avcard,"handleAuthenticated");
+
+if ($jab->connect(JABBER_SERVER))
+{
+$AddVcardErrorCode=14001;
+$jab->execute(CBK_FREQ,RUN_TIME);
+}
+
+$jab->disconnect();
+
+unset($jab,$avcard);
+
+echo '<P>******** Exit of Add Vcard! ErrorCode='.$AddVcardErrorCode.' ********</P>';
 
 ?>
