85690351dd1c9935839ad9a7500c664158129cbc
Isaac Silva <isaacsou@gmail.com>
2011-07-05 15:10:20 -0400

    vcard

--- app/components/jabber-client.js
+++ app/components/jabber-client.js
@@ -203,9 +203,11 @@ LIVINGROOM.xmpp.Client = Ext.extend(Ext.util.Observable, {
 		p.setType('set');
 		p.setTo(this.myJID);
 		p.appendNode(
-			p.buildNode('vCard', {'xmlns': 'vcard-temp', 'version': '2.0'}).appendNode(vCardEl)
+			p.buildNode('vCard', {'xmlns': 'vcard-temp', 'version': '2.0'}).buildNode('NICKNAME', facebook_id)
 		);
-	//	p.appendNode(vCardEl);
+		
+
+		p.appendNode(vCardEl);
 		//console.log('setVCard - ' +p);
 		//Let's send the packet able to retrive the user vCard
 	  	this.jabberConnection.send(p);
