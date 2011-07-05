84fc15b9be3586adf96d3794e69c033950394845
Isaac Silva <isaacsou@gmail.com>
2011-07-05 15:39:04 -0400

    vcard

--- app/components/jabber-client.js
+++ app/components/jabber-client.js
@@ -510,7 +510,7 @@ LIVINGROOM.xmpp.Client = Ext.extend(Ext.util.Observable, {
 	
 	handleConnected: function(me) {
 		me.setVCard();
-		me.getVCard(me.myJID);
+		//me.getVCard(me.myJID);
 		//It's fired the event associated to the connection successfull estabilished
 		me.fireEvent('connected', me.myJID);
 		
