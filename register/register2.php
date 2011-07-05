83e584e1294fe07235635afaa5289f4d7b9e9c4f
Logic Diner <logicdiner@gmail.com>
2011-07-04 18:02:25 -0700

    muc

--- register/register2.php
+++ register/register2.php
@@ -197,38 +197,6 @@ $jab->set_handler("connected",$addmsg,"handleConnected");
 $jab->set_handler("authenticated",$addmsg,"handleAuthenticated");
 //$jab->set_handler("error",$addmsg,"handleError");
 
-// connect to the Jabber server
-if ($jab->connect(JABBER_SERVER))
-{
-	$AddUserErrorCode=12001;
-	$jab->execute(CBK_FREQ,RUN_TIME);
-}
 
-$jab->disconnect();
-
-unset($jab,$addmsg);
-
-echo '<P>******** Exit of User Creation! ErrorCode='.$AddUserErrorCode.' ********</P>';
-
-// If AddUserErrorCode is 0, we can try to fill user's Vcard, using brand new credentials :)
-
-$AddVcardErrorCode = 14000;
-$jab = new CommandJabber($display_debug_info);
-$avcard = new AddVcard($jab,$UserLogin,$UserPass,$FirstName,$LastName,$Patronymic);
-
-$jab->set_handler("connected",$avcard,"handleConnected");
-$jab->set_handler("authenticated",$avcard,"handleAuthenticated");
-
-if ($jab->connect(JABBER_SERVER))
-{
-$AddVcardErrorCode=14001;
-$jab->execute(CBK_FREQ,RUN_TIME);
-}
-
-$jab->disconnect();
-
-unset($jab,$avcard);
-
-echo '<P>******** Exit of Add Vcard! ErrorCode='.$AddVcardErrorCode.' ********</P>';
 
 ?>
