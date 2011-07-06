ca483ebb7b3734ec5a633a5040fb720790fe4916
Logic Diner <logicdiner@gmail.com>
2011-07-05 22:09:45 -0700

    muc

---
+++ app/views/ChatSessionWrapper.js
@@ -0,0 +1,53 @@
+/* 
+ * LivingRoomAPI
+ */
+/**
+ * @class LivingRoomAPI.views.ChatSession
+ * @extends Ext.Panel
+ * ChatSession Screen
+ */
+LivingRoomAPI.views.ChatSessionWrapper = Ext.extend(Ext.Panel, {
+	
+	///@private
+	application: undefined,
+	
+	remoteJid: undefined,
+	
+	remoteUserName: undefined,
+	
+	jabberComponent: undefined,
+	
+	isChatRoom: false,
+	
+	initComponent: function(){
+
+
+		var blockOne = new Ext.Panel({
+			style:"background-color:#0000",
+			layout: 'auto',
+			padding: 15
+		})
+		
+		var blockTwo = new Ext.Panel({
+			style:"background-color:#ae2323",
+			layout: 'auto',
+			padding: 15
+		})
+
+		Ext.apply(this,{
+			layout:'vbox',
+			flex: 1,
+			fullscreen:'true',
+			items: [blockOne, blockTwo]
+		});
+
+		//Superclass inizialization
+		LivingRoomAPI.views.ChatSession.superclass.initComponent.call(this);
+	
+	}
+	
+});
+
+//Component type registration
+Ext.reg('ChatSessionWrapper', LivingRoomAPI.views.ChatSessionWrapper);
+
