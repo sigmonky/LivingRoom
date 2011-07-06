eb829ed1f99b2a1f671f00779855866719888ea0
Logic Diner <logicdiner@gmail.com>
2011-07-05 23:42:08 -0700

    muc

--- app/views/Viewport.js
+++ app/views/Viewport.js
@@ -59,7 +59,7 @@ LivingRoomAPI.Viewport = Ext.extend(Ext.TabPanel, {
 		});
 		
 		//Definition of the public chat panel
-		this.pnlPublicChat = new LivingRoomAPI.views.ChatSessionWrapper({
+		this.pnlPublicChat = new LivingRoomAPI.views.ChatSession({
 			itemId: 'pnlPublicChat',
 			title: 'Rooms',
 			isChatRoom: true,
