d49bd7a38a9432052605ed4a557ea787318bd396
Logic Diner <logicdiner@gmail.com>
2011-07-05 19:49:52 -0700

    muc

--- app/components/jabber-client.js
+++ app/components/jabber-client.js
@@ -601,8 +601,7 @@ LIVINGROOM.xmpp.Client = Ext.extend(Ext.util.Observable, {
 	
 	getRoasterComplete: function(iq, me){
 		console.log('getRoasterComplete');
-		cueca = new Debugger(4,’HelloWorld’);
-		cueca.start();
+
 		
 		if (!iq || iq.getType() != 'result') {
 			if (iq)
