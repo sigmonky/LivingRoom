6dc8b4169a4d7171ace17810aa7c2fc8cac96797
Logic Diner <logicdiner@gmail.com>
2011-07-05 22:26:10 -0700

    muc

--- app/views/ChatSessionWrapper.js
+++ app/views/ChatSessionWrapper.js
@@ -9,6 +9,8 @@
 LivingRoomAPI.views.ChatSessionWrapper = Ext.extend(Ext.Panel, {
 	
 	///@private
+	id: 'ChatSessionWrap',
+	
 	application: undefined,
 	
 	remoteJid: undefined,
@@ -24,18 +26,20 @@ LivingRoomAPI.views.ChatSessionWrapper = Ext.extend(Ext.Panel, {
 		var chatRoom = new LivingRoomAPI.views.ChatSession({
 			itemId: 'pnlTalent',
 			title: 'Talent',
+			id = 'test1',
 			isChatRoom: true,
 			jabberComponent: jabberClient
 		});
 		
 		var blockTwo = new Ext.Panel({
 			style:"background-color:#ae2323",
+			id = 'test2',
 			layout: 'auto',
 			padding: 115,
 		})
 
 		Ext.apply(this,{
-			layout:'fit',//* could be card as well?*///
+			layout:'card',//* could be card/fit as well?*///
 			flex: 1,
 			fullscreen:'true',
 			items: [chatRoom, blockTwo]
@@ -48,7 +52,7 @@ LivingRoomAPI.views.ChatSessionWrapper = Ext.extend(Ext.Panel, {
 	
 	showRoster: function(){
 		console.log('show roster');
-		Ext.getCmp('ChatSessionWrapper').setActiveItem(1);
+		Ext.getCmp('ChatSessionWrap').setCard('test2');
 	}
 	
 	
