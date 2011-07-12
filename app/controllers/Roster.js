af6bba93b0abeeb5996a54f8e5f48c22375c6769
Logic Diner <logicdiner@gmail.com>
2011-07-11 21:48:23 -0700

    facebook

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -113,6 +113,91 @@ Ext.regController('Roster', {
 		
 	},
 	
+	openChatSession: function(options){
+		
+		//Let's take all the user data
+		var user = options.user.data;
+
+		//Let's try to take an already active chat session panel
+		var pnlChatSession = this.application.viewport.getComponent('pnlRoster').getComponent(user.jid);
+		
+		if(pnlChatSession == undefined){
+		
+			//Let's create the chat session panel
+			var pnlChatSession = new LivingRoomAPI.views.ChatSession({
+				itemId: user.jid,
+				title: user.nickname,
+				iconCls: 'chat1',
+				iconMask: true,
+				badgeText: (options.show ? '' : '1'),
+				remoteJid: user.jid,
+				remoteUserName: user.nickname,
+				jabberComponent: facebookClient
+			});
+			
+			//Let's add the chat session panel
+			this.application.viewport.getComponent('pnlRoster').add(pnlChatSession);
+			
+			
+		}else{
+
+			
+			//Let's take the roster panel
+			var pnlRoster = this.application.viewport.getComponent('pnlRoster');
+			
+			//Taking the TabBar component
+			var tabBar = pnlRoster.getTabBar();
+
+			//Let's check if we are currently on the chat panel with this user
+			if(pnlRoster.getActiveItem() != pnlChatSession){
+				
+				//Let's check every open chat to find the right one inside the TabBar
+				Ext.each(tabBar.items.items, function(chat){
+					
+					//If the current chat panel is the one we are looking for...
+					if(chat.getText() == user.nickname) {
+						
+						//Let's take the actual badge text
+						var badgeNumber = (chat.getBadgeText() != '' ? parseInt(chat.getBadgeText()) : 0);
+						
+						//Let's increment the badge text
+						chat.setBadge(badgeNumber + 1);
+
+					}
+
+				}, this);
+
+			}
+
+		}
+		
+		
+		//Check if was requested to show the session panel
+		if(options.show){
+
+			//Let's show the chat session Panel
+			this.application.viewport.getComponent('pnlRoster').setActiveItem(pnlChatSession, {
+				type: 'slide', 
+				duration: 500
+			});
+
+		}
+
+		//Let's take the store that will contains all the roster users
+		var store = Ext.StoreMgr.get('OnlineUsers');
+
+		//Let's take the chat user
+		var user = store.getById(user.jid);
+		
+		//Saving the active chat session
+		user.set('chatActive', true);
+		
+	},
+	
+	
+	
+	
+	
 	addMessageToChatSession: function(options){
 		
 		//Let's take the store that will contains all the roster users
