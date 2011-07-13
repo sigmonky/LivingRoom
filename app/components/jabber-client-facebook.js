c7e4308184a046b0f659c0c1555bc6d2c9f9fbac
Isaac Silva <isaacsou@gmail.com>
2011-07-13 16:02:29 -0400

    facebook photo

--- app/components/jabber-client-facebook.js
+++ app/components/jabber-client-facebook.js
@@ -354,7 +354,10 @@ LIVINGROOM.xmpp.FacebookClient = Ext.extend(Ext.util.Observable, {
 
 			//Let's take the binval element containing the photo in base64 format
 			var binval = photo.getElementsByTagName('BINVAL')[0].textContent;
-
+			
+			console.log('photo is '+binval);
+			
+			
 			//Saving the photo mime type
 			user.set('photoType', type);
 		
