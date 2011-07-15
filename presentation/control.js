1aa84f295ab5039f02c51f0c8ac501bc6e1c1854
Isaac Silva <isaacsou@gmail.com>
2011-07-15 17:02:14 -0400

    added presentation

--- presentation/control.js
+++ presentation/control.js
@@ -56,7 +56,7 @@ function onConnect(status)
 
 
 $(document).ready(function () {
-    connection = new Strophe.Connection('http://www.logoslogic.com:5280/http-bind');
+    connection = new Strophe.Connection('http://www.logoslogic.com/http-bind');
     connection.rawInput = rawInput;
     connection.rawOutput = rawOutput;
 
