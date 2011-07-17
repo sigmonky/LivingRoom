abaca8c97396099efd074b96ab7c23d337f515f0
Logic Diner <logicdiner@gmail.com>
2011-07-16 21:26:59 -0700

    moderator

--- bbc/js/client2.js
+++ bbc/js/client2.js
@@ -132,7 +132,7 @@ $(document).ready(function () {
   Client.connection.rawInput = Client.raw_input;
   Client.connection.rawOutput = Client.raw_output;
 
-  Client.connection.connect('isaacueca@logoslogic.com','cigano', Client.on_connect);
+  Client.connection.connect('john@logoslogic.com','cigano', Client.on_connect);
 
   //Client.connection.connect(Config.XMPP_SERVER + '/pubsub','',Client.on_connect);
 });
