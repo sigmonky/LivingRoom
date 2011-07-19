1f12805e531a450df68146e6a3e26eb20f8f7e7b
Isaac Silva <isaacsou@gmail.com>
2011-07-19 17:38:29 -0400

    management and demo

--- talent/js/client2.js
+++ talent/js/client2.js
@@ -44,7 +44,7 @@ var Client = {
  //   $('#message').text(m);
 	var itemId = i;
 
-	$('#message').append('<div class="message-item" id='+itemId+'>' + m + '</div></div><br/><br/>');
+	$('#message').append('<div class="message-item" id='+itemId+'>' + m + '</div></div>');
   },
 
   // called when data is deemed as sent
