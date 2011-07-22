39a1f7b8e1faccf2d90edf59fe2c6af0bdc02435
Isaac Silva <isaacsou@gmail.com>
2011-07-22 16:06:54 -0400

    settins view

--- app/views/Settings.js
+++ app/views/Settings.js
@@ -12,40 +12,6 @@ LivingRoomAPI.views.Settings = new Ext.form.FormPanel({
 	initComponent : function(){
 		
 
-		Ext.apply(this,{
-		
-			fullscreen: true,
-			scroll: 'vertical',
-	      	items: [
-	         {
-	            xtype: 'fieldset',
-	            title: 'Settings',
-	            instructions: '',
-	            items: [
-	               {
-	                  xtype: 'textfield',
-	                  name: 'name',
-	                  label: 'Name',
-	                  placeHolder: 'Tom Roy',
-	                  autoCapitalize : true,
-	                  required: true,
-	                  useClearIcon: true
-	               },{
-	                   xtype: 'passwordfield',
-	                   name: 'password',
-	                   label: 'Password',
-	                   placeHolder: 'Select each time',
-	                   useClearIcon: true
-	               }, {
-		                  xtype: 'togglefield',
-		                  name: 'enable',
-		                  label: 'Auto Login'
-		            }
-	            ]
-	         }
-	      ]
-			
-		});
 
 		//Superclass inizialization
 		LivingRoomAPI.views.Roster.superclass.initComponent.call(this);
