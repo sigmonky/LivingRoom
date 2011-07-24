77b6018c3023cd1d35fc97328735ba2fb976eebb
Logic Diner <logicdiner@gmail.com>
2011-07-24 00:10:50 -0700

    panel action

--- app/views/RoomRosterView.js
+++ app/views/RoomRosterView.js
@@ -91,7 +91,7 @@ LivingRoomAPI.views.RoomRosterView = Ext.extend(Ext.Panel, {
 			store: 'RoomRoster',
             itemTpl: '<div class="x-roster-user">' +
 					    '<div class="x-user-picture">' +
-						'<img class="odd" src="https://graph.facebook.com/{facebook_id}/picture" width="32" height="32"/>' +
+						'<img class="odd" src="https://graph.facebook.com/{facebook_id}/picture" width="52" height="52"/>' +
 					     '</div>' +
 					 	'<div class="x-user-name">' +
 						 	'<b>{nickname}</b>' +
