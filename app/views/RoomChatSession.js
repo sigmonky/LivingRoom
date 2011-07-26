0db849f2a7d6b16924d2e5ac793f724d10d3e6e3
Logic Diner <logicdiner@gmail.com>
2011-07-26 02:18:45 -0700

    rooms store

--- app/views/RoomChatSession.js
+++ app/views/RoomChatSession.js
@@ -332,8 +332,8 @@ LivingRoomAPI.views.RoomChatSession = Ext.extend(Ext.Panel, {
 		
 		console.log('2');
 		
-		user = roster.getById(from);
-		var photo = user.get('facebook_id');
+		//user = roster.getById(from);
+		//var photo = user.get('facebook_id');
 		var photo = null;
 	//	console.log('photo ='+photo);
 		console.log('3');
