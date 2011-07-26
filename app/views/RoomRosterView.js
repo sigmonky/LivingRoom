260d305a3714feb2fe4201b41f33b11b1d5b78a4
Logic Diner <logicdiner@gmail.com>
2011-07-26 02:33:49 -0700

    rooms store

--- app/views/RoomRosterView.js
+++ app/views/RoomRosterView.js
@@ -194,10 +194,10 @@ LivingRoomAPI.views.RoomRosterView = Ext.extend(Ext.Panel, {
      */
 	updateStore: function(){
 		
-		var itemListGallery = Ext.getCmp('itemListGallery');
-        var newStore = Ext.StoreMgr.get(this.key);
-        itemListGallery.update();
-        itemListGallery.bindStore(newStore);
+	//	var itemListGallery = Ext.getCmp('itemListGallery');
+      //  var newStore = Ext.StoreMgr.get(this.key);
+      //  itemListGallery.update();
+       // itemListGallery.bindStore(newStore);
 
 	},
 
