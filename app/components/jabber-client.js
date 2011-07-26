5d2ec8009b06cc8022d22fb3ade878174780bdc7
Isaac Silva <isaacsou@gmail.com>
2011-07-26 16:06:45 -0400

    mutliple chat

--- app/components/jabber-client.js
+++ app/components/jabber-client.js
@@ -564,7 +564,7 @@ LIVINGROOM.xmpp.Client = Ext.extend(Ext.util.Observable, {
 	joinPublicRoom: function(publicRoomName){
 			
 		if (Ext.StoreMgr.get(this.roomRoster) != undefined){
-			Ext.StoreMgr.get(this.roomRoster).removeAll();
+			//Ext.StoreMgr.get(this.roomRoster).removeAll();
 		}
 		
 		
