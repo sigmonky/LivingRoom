8f35162370d7d245e2379c95b35e1889736de668
Logic Diner <logicdiner@gmail.com>
2011-07-26 01:27:26 -0700

    rooms store

--- app/components/jabber-client.js
+++ app/components/jabber-client.js
@@ -622,7 +622,7 @@ LIVINGROOM.xmpp.Client = Ext.extend(Ext.util.Observable, {
 		console.log('joinRoomComplete myJID is ' + me.myJID);
 		console.log('joinRoomComplete fb_id is ' + fb_id);
 		console.log('joinRoomComplete me.nickname ' + me.nickname);
-		console.log('joinRoomComplete me.roomJid ' + this.roomJid);
+		console.log('joinRoomComplete me.roomJid ' + me.roomJid);
 
 		var item = Ext.ModelMgr.create({
 		    jid: me.roomJid+'/'+me.nickname,
