f5c667b9a4e11f13b06f924f61fe878a323ff971
Logic Diner <logicdiner@gmail.com>
2011-07-28 01:43:56 -0700

    thumbnail photos

--- app/models/RoomRosterItem.js
+++ app/models/RoomRosterItem.js
@@ -7,6 +7,7 @@ Ext.regModel('RoomRosterItem', {
     fields: [				
         {name: 'jid', type: 'string'},
 		{name: 'nickname', type: 'string'},
-        {name: 'facebook_id', type: 'string'}
+        {name: 'facebook_id', type: 'string'},
+        {name: 'profile_thumb_url', type: 'string'},
     ]
 });
