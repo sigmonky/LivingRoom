da4fb61675faab82b393a213f26bdee49b1b5ce4
Isaac Silva <isaacsou@gmail.com>
2011-08-04 16:29:17 -0400

    thumbnail photos

--- app/views/RoomChatSession.js
+++ app/views/RoomChatSession.js
@@ -177,13 +177,10 @@ LivingRoomAPI.views.RoomChatSession = Ext.extend(Ext.Panel, {
 							var user = store.getAt(index);
 							console.log('itemtap user =' +user);
 
-	
-
-
 							var tplUser = new Ext.XTemplate(
 								'<tpl for=".">',
 									'<div style="padding:20px"><div class="x-user-picture">' +
-										'<img src="https://graph.facebook.com/{facebook_id}/picture" width="52" height="52"/>'+
+										'<img src="{photo_url}" width="52" height="52"/>'+
 									'</div>' +
 								     '<div class="x-user-name">' +
 										'<p class="nickname">{nickname}</p>' +
@@ -191,10 +188,17 @@ LivingRoomAPI.views.RoomChatSession = Ext.extend(Ext.Panel, {
 								'</tpl>'
 							);
 
+							var facebook_id = user.get('facebook_id')
+
+							if (facebook_id != ''){
+								var photo_url = "https://graph.facebook.com/"+facebook_id+"/picture";
+							}else{
+								var photo_url  = 'http://www.logoslogic.com/chat/LivingRoom/user_default.gif';
+							}
 
 							var html = tplUser.apply({
 								nickname: user.get('nickname'),
-				            	facebook_id: user.get('facebook_id')
+				            	photo_url: photo_url,
 				        	});
 
 						//	overlay.html = html;
