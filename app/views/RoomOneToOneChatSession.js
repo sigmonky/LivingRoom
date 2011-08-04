48ac9580989c9d5004b2a083a3a16bbf0369cdc7
Isaac Silva <isaacsou@gmail.com>
2011-08-04 12:53:40 -0400

    black background

--- app/views/RoomOneToOneChatSession.js
+++ app/views/RoomOneToOneChatSession.js
@@ -404,9 +404,10 @@ LivingRoomAPI.views.RoomOneToOneChatSession = Ext.extend(Ext.Panel, {
 	
 	getMyFacebooKProfilePhoto: function(){
 		var facebookStore = Ext.StoreMgr.get('FacebookUser');
-		console.log('facebookStore = '+facebookStore);
-		if (facebookStore != ''){
-			var obj = facebookStore.getAt(0);
+		var obj = facebookStore.getAt(0);
+		if (obj != ''){
+			console.log('obj = '+obj);
+
 			var facebook_id = obj.get('id');
 			console.log('facebok id ' +facebook_id);
 			return facebook_id; 
