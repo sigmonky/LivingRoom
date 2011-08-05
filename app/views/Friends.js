618a4479c9f17daffbb3ae81874e03b566570176
Isaac Silva <isaacsou@gmail.com>
2011-08-05 17:04:35 -0400

    added friends data model

--- app/views/Friends.js
+++ app/views/Friends.js
@@ -81,7 +81,7 @@ LivingRoomAPI.views.Friends = Ext.extend(Ext.Panel, {
 			    proxy: {
 			        type: 'ajax',
 			      //  url: 'https://graph.facebook.com/me/friends?access_token='+getFacebookTokenFromUrl(),
-					url: 'https://graph.facebook.com/me/friends?access_token=185799971471968%7Ce83f2eff9c114736aac52c0b.3-527305423%7C_DlATFHB_CJa2hlpSxwDGbCaYEE'
+					url: 'https://graph.facebook.com/me/friends?access_token=185799971471968%7Ce83f2eff9c114736aac52c0b.3-527305423%7C_DlATFHB_CJa2hlpSxwDGbCaYEE',
 			        reader: {
 			            type: 'json',
 			        }
