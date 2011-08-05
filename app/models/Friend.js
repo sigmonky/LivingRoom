4b4400fe49973247a7e2d854c767f9f085ca6a9f
Isaac Silva <isaacsou@gmail.com>
2011-08-05 17:03:12 -0400

    added friends data model

--- app/models/Friend.js
+++ app/models/Friend.js
@@ -6,8 +6,8 @@ Ext.regModel('Friend', {
 	idProperty: 'jid',
     fields: [				
         {name: 'jid', type: 'string'},
+        {name: 'id', type: 'string'},
 		{name: 'name', type: 'string'},
-        {name: 'facebook_id', type: 'string'},
         {name: 'thumb', type: 'string'},
 		{name: 'isLive', type: 'bool'},
 		{name: 'didInstallApp', type: 'bool'},
