0cd2fbf986a4d9bb6f038fc2f535a1723480134b
Isaac Silva <isaacsou@gmail.com>
2011-08-05 17:23:34 -0400

    added friends data model

--- app/views/Friends.js
+++ app/views/Friends.js
@@ -109,8 +109,8 @@ LivingRoomAPI.views.Friends = Ext.extend(Ext.Panel, {
 						console.log('data =' +data.results);
 						
 						var friendStore = Ext.StoreMgr.get('FriendListStore');
-						console.log('data.length ='+data.data.length);
-					    for (var i = 0, ln = data.data.length; i < ln; i++) {
+						console.log('data.length ='+data.results);
+					    for (var i = 0, ln = data.results.length; i < ln; i++) {
 						
 	                        var friend = data[i];
 							var friend = Ext.ModelMgr.create({id: data.id, name: data.name}, 'Friend');
