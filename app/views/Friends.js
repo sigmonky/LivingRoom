0a7c9363d4abb242679eb017f07cc34902b0eda7
Isaac Silva <isaacsou@gmail.com>
2011-08-08 16:13:42 -0400

    friends dialog box

--- app/views/Friends.js
+++ app/views/Friends.js
@@ -239,7 +239,7 @@ LivingRoomAPI.views.Friends = Ext.extend(Ext.Panel, {
 				    // Callback
 				    callback: function (data) {
 						console.log('data.length ='+data.data.length);
-						that.store.loadData(Ext.util.JSON.decode(data.results.data))
+						that.store.loadData(Ext.util.JSON.decode(data.data))
 						
 						var allFriends = data;
 						
