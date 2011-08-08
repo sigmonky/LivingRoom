484a6e1b10dfdaa97fc0b000372f12143c57fa62
Isaac Silva <isaacsou@gmail.com>
2011-08-08 16:28:19 -0400

    friends dialog box

--- app/views/Friends.js
+++ app/views/Friends.js
@@ -22,7 +22,17 @@ LivingRoomAPI.views.Friends = Ext.extend(Ext.Panel, {
 						    	type: 'json'
 						   	}
 						},	
-
+				sorters: 'name', 			    
+				getGroupString : function(record) {
+						/*	var didInstallApp = record.get('didInstallApp');
+							if (didInstallApp == 'yes'){
+								var str = 'Invite More Friends To Chat';
+							}else{
+								var str = 'My Facebook Friends';
+							} */
+							
+					        return  record.get('name')[0];
+				},
 				autoLoad:false
 
 			});
@@ -51,7 +61,7 @@ LivingRoomAPI.views.Friends = Ext.extend(Ext.Panel, {
 					 	'<img src="https://graph.facebook.com/{id}/picture" width="32" height="32" />' +
 					     '</div>' +
 					 	'<div class="x-user-name">' +
-						 	'<b>{id}</b>' +
+						 	'<b>{name}</b>' +
 					     '</div>' +
 					  '</div>',
 			listeners: {
@@ -230,10 +240,61 @@ LivingRoomAPI.views.Friends = Ext.extend(Ext.Panel, {
 				    callback: function (data) {
 						console.log('data.length ='+data.data.length);
 						
-						that.store.loadData(data.data)
-						that.store.sync();
 						
-		
+						var allFriends = data;
+						
+						var friendsWhoInstalledApp = new Array();
+						
+						Ext.util.JSONP.request({
+					    		url: 'https://api.facebook.com/method/fql.query',
+								params: {
+									access_token: '185799971471968%7Ce83f2eff9c114736aac52c0b.3-527305423%7C_DlATFHB_CJa2hlpSxwDGbCaYEE',
+									query: 'SELECT uid,name,pic_square FROM user WHERE is_app_user AND uid IN (SELECT uid2 FROM friend WHERE uid1 = me())',
+									format: 'JSON',
+								},
+								
+							    callbackKey: 'callback',
+							    // Callback
+							    callback: function (data2) {
+									console.log('data2.length ='+data2.length);
+								    for (var i = 0, ln = data2.length; i < ln; i++) {
+				                        var friend = data2[i];
+										console.log('friendWhoInstalled.name '+friend.name);
+										friendsWhoInstalledApp.push(friend);
+				                    }
+				
+									console.log('friendsWhoInstalledApp lenght' +friendsWhoInstalledApp.length);
+
+								    for (var i = 0, ln = allFriends.data.length; i < ln; i++) {
+										var didInstall = false;
+				                        var friend = allFriends.data[i];
+				
+
+									    for (var j = 0, ln2 = friendsWhoInstalledApp.length; j < ln2; j++) {
+											var friendWhoInstalled = friendsWhoInstalledApp[j];
+											if (friendWhoInstalled.uid == friend.id){
+												console.log('friendWhoInstalled.id == friend.id');
+												didInstall = true;
+											}
+										}
+										if (didInstall == true){
+											var friendModel = Ext.ModelMgr.create({id: friend.id, name: friend.name, didInstallApp: true, thumb:'a'}, 'Friend');
+										}else{
+											var friendModel = Ext.ModelMgr.create({id: friend.id, name: friend.name, didInstallApp: false, thumb:'b'}, 'Friend');
+										}
+
+										that.store.add(friendModel);
+								    //	that.store.sync();
+
+										//didInstallApp
+
+										loadingMask.hide();
+
+
+										
+				                    }
+							  	}	
+						});
 						
 
 				  	}	
@@ -242,19 +303,16 @@ LivingRoomAPI.views.Friends = Ext.extend(Ext.Panel, {
 			
 			var itemSubList = Ext.getCmp('friendsList');
 	        itemSubList.update();
+			this.store.sync();
 	        
-		//	itemSubList.store.loadData(this.store.data.items)
+			itemSubList.store.loadData(this.store.data.items)
 			//this.store = friendStore;
-	       // itemSubList.update();
+	        itemSubList.update();
 	
 	        itemSubList.bindStore(this.store);
-		//	itemSubList.refresh();
+			itemSubList.refresh();
 	
 			this.isLoaded = true;
-			
-			
-			
-			
 		/*	Ext.regStore('FriendListStore', {
 				model: 'Friend',
 				autoLoad: true,
