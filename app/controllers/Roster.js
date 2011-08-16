dc5431b768edcfed3dfda3e7f36fd6258791bb27
Isaac Silva <isaacsou@gmail.com>
2011-08-16 11:50:11 -0400

    chat popup invitation

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -588,12 +588,12 @@ Ext.regController('Roster', {
 							
 							var tplUser = new Ext.XTemplate(
 								'<tpl for=".">',
-									'<div class="x-user-picture">' +
+									'<div class="x-popup-invite"><div class="x-user-picture">' +
 										'<img src="{photo_url}" width="52" height="52"/>'+
 									'</div>' +
 								     '<div class="x-user-name">' +
 										'<b>{name} is inviting you to chat</b>' +
-									  '</div>' +
+									  '</div></div>' +
 								'</tpl>'
 							);
 							
