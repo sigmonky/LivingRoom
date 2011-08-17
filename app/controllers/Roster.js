6fa5e1417a7e05155291ee6b64c17a5ff4500f9c
Logic Diner <logicdiner@gmail.com>
2011-08-17 00:25:21 -0700

    one to one

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -164,7 +164,7 @@ Ext.regController('Roster', {
 			});
 		
 		}else{
-				cueca.each(function (record) {
+				chatStore.each(function (record) {
 				    console.log('addMessageToOneToOneChatSession store each message = '+record.get('message'));
 				});
 		}
@@ -641,7 +641,7 @@ Ext.regController('Roster', {
 	
 						}
 						
-						cueca = Ext.StoreMgr.get(keyMsg);
+						chatStore = Ext.StoreMgr.get(keyMsg);
 						console.log('addMessageToChatRoom chatStore2 '+chatStore);
 						
 						chatStore.add(message);
