747c0c21dec9a37b3ec7b4048da11dfebb656d12
Logic Diner <logicdiner@gmail.com>
2011-08-17 01:12:38 -0700

    one to one

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -142,6 +142,7 @@ Ext.regController('Roster', {
 		
 		var userRemoteJidMsg = user.get('jid') +'_message';
 		
+		console.log('openChatSessionOneToOne pnlChatSession ='+pnlChatSession)
 		
 		console.log('openChatSessionOneToOne userRemoteJidMsg ='+userRemoteJidMsg)
 		
