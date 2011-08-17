20f1e0ff439db4ee2cd2b61b65a72f6b9a203996
Logic Diner <logicdiner@gmail.com>
2011-08-17 01:11:01 -0700

    one to one

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -649,7 +649,9 @@ Ext.regController('Roster', {
 
 						console.log('addMessageToChatRoom chatStore2 '+chatStore);
 						
+						var chatStore = Ext.StoreMgr.get(keyMsg);
 
+						chatStore.add(message);
 				
 						if (isActive == false){
 							
@@ -674,9 +676,7 @@ Ext.regController('Roster', {
 							
 							pnlChatSession.doUpdate();
 							
-							var chatStore = Ext.StoreMgr.get(keyMsg);
 
-						//	chatStore.add(message);
 							
 							console.log('addMessageToChatRoom is not active ');
 					
