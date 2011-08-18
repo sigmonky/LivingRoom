2519d1b07a1369c1881d9bdc597ffe9325c09153
Isaac Silva <isaacsou@gmail.com>
2011-08-18 15:34:21 -0400

    invite friends

--- app/views/InviteFbFriends.js
+++ app/views/InviteFbFriends.js
@@ -281,7 +281,7 @@ LivingRoomAPI.views.InviteFbFriends = Ext.extend(Ext.Panel, {
 					duration: 250
 				},
 				listeners:{
-					activate : function(){
+					beforeactivate : function(){
 						console.log('activate popup');
 						Ext.Msg.alert("Success","listener working successfully",Ext.emptyFn);
 						that.ShowWebPageInPanel(this,"https://www.facebook.com/dialog/send?app_id=185799971471968&name=People%20Argue%20Just%20to%20Win&link=http://www.nytimes.com/2011/06/15/arts/people-argue-just-to-win-scholars-assert.html&redirect_uri=http://www.logoslogic.com/chat/LivingRoom");						
