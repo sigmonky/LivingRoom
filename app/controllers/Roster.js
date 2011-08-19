d22402ec3dad582c65c7810cf4ccdcecfe9dca4d
Isaac Silva <isaacsou@gmail.com>
2011-08-19 14:43:45 -0400

    notification on menu

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -545,9 +545,9 @@ Ext.regController('Roster', {
 	
 	resetBadge: function(){
 		var tabBarItem = this.application.viewport.getTabBar().items.items[1];
-		if (currentScreen == 'friends'){
+		
+ 		if (currentScreen == 'friends'){
 			tabBarItem.setBadge('');
-			
 		}
 		
 	},
@@ -608,7 +608,17 @@ Ext.regController('Roster', {
 			}else{
 						console.log('current screen '+currentScreen);
 						
-						this.resetBadge();
+						var tabBarItem = this.application.viewport.getTabBar().items.items[1];
+						
+						if (currentScreen != 'friends'){
+							console.log('get tab bar'+this.application.viewport.getTabBar().items.items[1].getBadgeText());
+							console.log('addMessageToChatRoom - badgeText = '+tabBarItem.getBadgeText());
+						    var badgeNumber = (tabBarItem.getBadgeText() != '' ? parseInt(tabBarItem.getBadgeText()) : 0);
+							tabBarItem.setBadge(badgeNumber + 1);
+						}else if (currentScreen == 'friends'){
+							tabBarItem.setBadge('');
+
+						}
 						
 						console.log('addMessageToOneToOneChatSession options.from '+ options.from);
 						console.log('addMessageToOneToOneChatSession options.message '+ options.message);
