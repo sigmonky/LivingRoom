fcb5a8e75e583ec96d1a8b2cf2259105a4f97750
Isaac Silva <isaacsou@gmail.com>
2011-08-19 14:35:10 -0400

    notification on menu

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -543,17 +543,7 @@ Ext.regController('Roster', {
 		});
 	},
 	
-	resetBadge: function(){
-		var tabBarItem = this.application.viewport.getTabBar().items.items[1];
-		if (currentScreen != 'friends'){
-			console.log('get tab bar'+this.application.viewport.getTabBar().items.items[1].getBadgeText());
-			console.log('addMessageToChatRoom - badgeText = '+tabBarItem.getBadgeText());
-		    var badgeNumber = (tabBarItem.getBadgeText() != '' ? parseInt(tabBarItem.getBadgeText()) : 0);
-			tabBarItem.setBadge(badgeNumber + 1);
-		}else if (currentScreen == 'friends'){
-			tabBarItem.setBadge('');
-		}
-	},
+	resetBadge: function(){},
 	
 	addMessageToChatRoom: function(options){
 		
@@ -611,7 +601,17 @@ Ext.regController('Roster', {
 			}else{
 						console.log('current screen '+currentScreen);
 						
-					//	this.resetBadge();
+						var tabBarItem = this.application.viewport.getTabBar().items.items[1];
+
+						if (currentScreen != 'friends'){
+							console.log('get tab bar'+this.application.viewport.getTabBar().items.items[1].getBadgeText());
+							console.log('addMessageToChatRoom - badgeText = '+tabBarItem.getBadgeText());
+						    var badgeNumber = (tabBarItem.getBadgeText() != '' ? parseInt(tabBarItem.getBadgeText()) : 0);
+							tabBarItem.setBadge(badgeNumber + 1);
+						}else if (currentScreen == 'friends'){
+							tabBarItem.setBadge('');
+							
+						}
 						
 						
 						console.log('addMessageToOneToOneChatSession options.from '+ options.from);
