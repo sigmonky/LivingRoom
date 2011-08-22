4420770d5681743f794180c3afc54169652815e7
Isaac Silva <isaacsou@gmail.com>
2011-08-22 17:23:28 -0400

    friend popup selector

--- app/views/RoomChatSession.js
+++ app/views/RoomChatSession.js
@@ -67,7 +67,7 @@ LivingRoomAPI.views.RoomChatSession = Ext.extend(Ext.Panel, {
 				id: 'showRoster',
 				text: 'Co-Viewers',
 				iconMask: true,
-				iconCls: 'locate',
+				iconCls: 'search',
 				scope: this,
 				handler: this.showRoster
 			},
