2c8b851d07426e598e8959ff4aab4304ae54900f
Logic Diner <logicdiner@gmail.com>
2011-08-22 23:52:16 -0700

    orientation change

--- app/controllers/Roster.js
+++ app/controllers/Roster.js
@@ -249,9 +249,9 @@ Ext.regController('Roster', {
 	
 	orientationChange: function(){
 		console.log('RoomChatSession getDockedComponent 1 ='+this.application.viewport.getComponent('pnlFriends'));
-		console.log('RoomChatSession getDockedComponent 2 ='+this.application.viewport.getComponent('pnlFriends').getDockedComponent('msgToolbar'));
-		
-		console.log('RoomChatSession getDockedComponent 3 ='+this.application.viewport.getComponent('pnlFriends').getDockedComponent('msgToolbar').getComponent('message'));
+		// console.log('RoomChatSession getDockedComponent 2 ='+this.application.viewport.getComponent('pnlFriends').getDockedComponent('msgToolbar'));
+		this.application.viewport.getComponent('pnlFriends')).orientationChangeAction();
+		// console.log('RoomChatSession getDockedComponent 3 ='+this.application.viewport.getComponent('pnlFriends').getDockedComponent('msgToolbar').getComponent('message'));
 		
 	},
 	
