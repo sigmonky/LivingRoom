cf3c143bacb773a12f59361086843bfbc2a9f605
Logic Diner <logicdiner@gmail.com>
2011-08-22 22:57:20 -0700

    orientation change

--- app/views/RoomChatSession.js
+++ app/views/RoomChatSession.js
@@ -261,7 +261,10 @@ LivingRoomAPI.views.RoomChatSession = Ext.extend(Ext.Panel, {
 
 		//Let's take the written message
 		var message = this.getDockedComponent('msgToolbar').getComponent('message');
-
+		
+		console.log('RoomChatSession getDockedComponent ='+this.getDockedComponent('msgToolbar'));
+		
+		console.log('RoomChatSession message ='+message);
 		//Let's hide the virtual keyboard
 		message.fieldEl.dom.blur();
 
