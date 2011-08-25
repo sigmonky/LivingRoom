c939ef7340add83b305f581abc11d6763fa4a87b
Logic Diner <logicdiner@gmail.com>
2011-08-24 23:48:56 -0700

    facebook callback

--- southpark/cueca.php
+++ southpark/cueca.php
@@ -25,69 +25,44 @@ class Xmpp_Bosh
         $hash = base64_encode( $user . "@logoslogic.com\0" . $user ."\0" . $password ) . "\n";
         $rid = rand();
         $jid = $user . "@logoslogic.com";
-        // when using openfire
-//         //$body = "<body rid='" . $rid . "' xmlns='http://jabber.org/
-// protocol/httpbind' to='logoslogic.com' xml:lang='en' wait='60' hold='1'
-// window='5' content='text/xml; charset=utf-8' ver='1.6'
-// xmpp:version='1.0' xmlns:xmpp='urn:xmpp:xbosh'/>";
 
-        //when using punjab
-        $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/
-protocol/httpbind' to='logoslogic.com' route='xmpp:logoslogic.com:5222'
-xml:lang='en' wait='60' hold='1' window='5' content='text/xml;
-charset=utf-8' ver='1.6' xmpp:version='1.0'
-xmlns:xmpp='urn:xmpp:xbosh'/>";
-        $return = $this->__sendBody( $body );
-        $xml = new SimpleXMLElement( $return );
-
-        $sid = $xml['sid'];
-        $rid ++;
-        $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/
-protocol/httpbind' sid='" . $sid . "'><auth
-xmlns='urn:ietf:params:xml:ns:xmpp-sasl' mechanism='PLAIN'>" . $hash .
-"</auth></body>";
-        $return = $this->__sendBody( $body );
-
-        $rid ++;
-        $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/
-protocol/httpbind' sid='" . $sid . "' to='logoslogic.com' xml:lang='en'
-xmpp:restart='true' xmlns:xmpp='urn:xmpp:xbosh'/>";
-        $return = $this->__sendBody( $body );
-
-        $rid ++;
-        $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/
-protocol/httpbind' sid='" . $sid . "'><iq type='set' id='_bind_auth_2'
-xmlns='jabber:client'><bind xmlns='urn:ietf:params:xml:ns:xmpp-bind'/
-
-></iq></body>";
+		$body = "<body content='text/xml; charset=utf-8' hold='1' xmlns='http://jabber.org/protocol/httpbind' to='logoslogic.com' wait='300' rid='" . $rid . "' route='xmpp:logoslogic.com:5222' secure='false'  ver='1.6' xmlns:xmpp='urn:xmpp:xbosh' xmpp:version='1.0'/>";
 
         $return = $this->__sendBody( $body );
-
-        $rid ++;
-        $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/
-protocol/httpbind' sid='" . $sid . "'><iq type='set'
-id='_session_auth_2' xmlns='jabber:client'><session
-xmlns='urn:ietf:params:xml:ns:xmpp-session'/></iq></body>";
-        $return = $this->__sendBody( $body );
-        $rid ++;
-        $this->rid = $rid;
-        $this->sid = $sid;
-        $this->jid = $jid;
+         $xml = new SimpleXMLElement( $return );
+        
+                $sid = $xml['sid'];
+                $rid ++;
+                $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/protocol/httpbind' sid='" . $sid . "'><authxmlns='urn:ietf:params:xml:ns:xmpp-sasl' mechanism='PLAIN'>" . $hash ."</auth></body>";
+                $return = $this->__sendBody( $body );
+        
+                $rid ++;
+                $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/protocol/httpbind' sid='" . $sid . "' to='logoslogic.com' xml:lang='en' xmpp:restart='true' xmlns:xmpp='urn:xmpp:xbosh'/>";
+                $return = $this->__sendBody( $body );
+        
+                $rid ++;
+                $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/protocol/httpbind' sid='" . $sid . "'><iq type='set' id='_bind_auth_2' xmlns='jabber:client'><bind xmlns='urn:ietf:params:xml:ns:xmpp-bind'/></iq></body>";
+        
+                $return = $this->__sendBody( $body );
+        
+                $rid ++;
+                $body = "<body rid='" . $rid . "' xmlns='http://jabber.org/protocol/httpbind' sid='" . $sid . "'><iq type='set' id='_session_auth_2' xmlns='jabber:client'><session xmlns='urn:ietf:params:xml:ns:xmpp-session'/></iq></body>";
+                $return = $this->__sendBody( $body );
+                $rid ++;
+                $this->rid = $rid;
+                $this->sid = $sid;
+                $this->jid = $jid;
     }
 
     private function __sendBody ( $body )
     {
-        //when using openfire
-        //$ch = curl_init( "http://www.logoslogic.com/http-bind" );
 
-        //when using punjab
         $ch = curl_init( "http://www.logoslogic.com/http-bind" );
         curl_setopt( $ch , CURLOPT_HEADER , 0 );
         curl_setopt( $ch , CURLOPT_POST , 1 );
         curl_setopt( $ch , CURLOPT_POSTFIELDS , $body );
         curl_setopt( $ch , CURLOPT_FOLLOWLOCATION , true );
-        $header = array(/*'Accept-Encoding: gzip, deflate',*/'Content-
-Type: text/xml; charset=utf-8'
+        $header = array(/*'Accept-Encoding: gzip, deflate',*/'Content-Type: text/xml; charset=utf-8'
         );
         curl_setopt( $ch , CURLOPT_HTTPHEADER , $header );
         curl_setopt( $ch , CURLOPT_VERBOSE , 0 );
@@ -103,19 +78,204 @@ Type: text/xml; charset=utf-8'
 }
 
 $connection = new Xmpp_Bosh( );
-$connection->connect( "isaacueca@logoslogic.com" , "cigano" );
+
+$connection->connect( "isaacueca" , "cigano" );
 $rid = $connection->getRid();
 $jid = $connection->getJid();
 $sid = $connection->getSid();
+
 ?>
 
-... YOUR CODE HERE
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>South Park</title>
+		
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+		<link href="styles/global.css" rel="stylesheet" type="text/css" />
+		
+		<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/themes/flick/jquery-ui.css">
+		<script src="libs/jquery-1.4.2.min.js"></script>
+		<script src="libs/jquery-ui-1.8.2.custom.min.js"></script>
+		<script src="libs/jquery.inlineEdit.js" type="text/javascript"></script>
+		<script src="libs/strophe.js" type="text/javascript"></script>
+		<script src="libs/strophe.roster.js" type="text/javascript"></script>
+		<script src="libs/strophe.status.js" type="text/javascript"></script>
+		<script src="libs/strophe.chat.js" type="text/javascript"></script>
+		<script src="libs/strophe.muc.js" type="text/javascript"></script>
+		<script src="libs/mustache.js" type="text/javascript"></script>
+		<script src="otalk.js" type="text/javascript"></script>
+		<script src="ICanHaz.js" type="text/javascript"></script>
+		
+		<script src="libs/backbone.js"></script>
+		<script src="libs/underscore.js"></script>
+		
+		<script type="text/javascript">
+			
+			// our global config object
+			// plugins use this if it exists
+			var StropheConfig = {
+				// Settings
+				boshUrl: 'http://www.logoslogic.com/http-bind',
+				
+				// Implemented event handlers
+				subscriptionRequested: otalk.subscription_requested,
+				chatReceived: otalk.on_chat_message,
+				rosterChanged: otalk.update_roster,
+				
+				// Not implemented in UI
+				handleMucMessage: otalk.handle_muc_message,
+				chatStateReceived: otalk.chat_state_received
+			};
+			
+			$(function () {
+				// init our app
+				otalk.init();
+			});
+		</script>
+		
+		<!-- Our html Mustache.js templates all go below. (Yes this validates) -->
+		<script id="user" type="text/html">
+			<li class="user {{ status }}" id="roster_{{ jid_id }}" data-jid="{{ jid }}">
+				<span class="name">{{ name }}</span><a href="#" class="edit_user">edit</a>
+				<span class="status">{{#status_message}} - {{ status_message }}{{/status_message}}</span>
+				<span class="jid">{{ jid }}</span>
+				<span>
+					<a href="#" class="remove">remove</a>
+					<a href="#" class="start_chat">chat</a>
+				</span>
+			</li>
+		</script>
+		
+		<script id="group" type="text/html">
+			<div class="group" title="{{ title }}">
+				<span class="group_title">{{{ title }}}</span>
+				<ul></ul>
+			</div>
+		</script>
+		
+		<script id="add_user_dialog" type="text/html">
+			<div>
+				<label>JID:</label><input type="text" id="add_user_jid">
+				<label>Name:</label><input type="text" id="add_user_name">
+			</div>
+		</script>
 
-var conn = new Strophe.Connection(
-         'http://127.0.0.1/xmpp-httpbind');
-   conn.connect.attach(<?=$jid?>, <?=$rid?>, <?=$sid?>, null);
-The class
-----------------------------------
+		<script id="edit_user_dialog" type="text/html">
+			<div>
+				<label>Name:</label><input type="text" id="add_user_name" value="{{ name }}">
+			</div>
+		</script>
+		
+		<script id="subscription_request" type="text/html">
+			<div>
+				<p>Subscription request received from {{ from }}</p>
+				<label>Name:</label><input type="text" id="add_user_name">
+			</div>
+		</script>
+		
+		<script id="chat_window" type="text/html">
+			<ul class="chat_list"></ul>
+			<input class="chat_input" id="input_{{ id }}" type="text" />
+		</script>
+		
+		<script id="muc_window" type="text/html">
+			<ul class="chat_list"></ul>
+			<input class="muc_input" id="input_{{ id }}" type="text" />
+			<ul class="roster"></ul>
+		</script>
+		
+		<script id="chat_message" type="text/html">
+			<li class="chat_message{{#from}} {{from}}{{/from}}">
+				<span class="chat_name">{{ name }}</span>
+				<span class="chat_text">{{{ message }}}</span>
+			</li>
+		</script>
+		
+		<script id="muc_dialog" type="text/html">
+			<div>
+				<label>Room:</label><input type="text" id="muc_room"/>
+				<label>Nickname:</label><input type="text" id="muc_nickname"/>
+			</div>
+		</script>
+	</head>
+	  <!-- 
+         var Attacher = {
+             JID: '<?=$jid?>',
+             SID: '<?=$rid?>,
+             RID: '<?=$sid?>'
+         };
+         // -->
+    </script>
+
+
+       <button id="fb-auth">Login</button>
+        <div id="loader" style="display:none">
+            <img src="ajax-loader.gif" alt="loading" />
+        </div>
+		<br/>
+
+
+		<div class="toolbar">
+			<input id="disconnect" type="button" value="disconnect" disabled="disabled">
+			<button id="add_contact">Add Contact</button>
+			<select id="show">
+				<option value="">Available</option>
+				<option value="chat">Free for chat</option>
+				<option value="away">Away</option>
+				<option value="xa">Extended Away</option>
+				<option value="dnd">Do Not Distrub</option>
+			</select>
+			<label>Custom Status:</label><span id="custom_status"></span>
+			<button id="join_muc">Join MUC</button>
+		</div>
+
+		<section id="roster">
+
+		</section>
+
+		<section id="chats">
+			<ul></ul>
+		</section>
+
+		<!-- Dialogs Below -->
+		<div id="login_dialog" class="hidden">
+			<label>JID:</label><input type="text" id="jid">
+			<label>Password:</label><input type="password" id="password">
+		</div>
+
+		<div id="contact_dialog" class="hidden">
+			<label>JID:</label><input type="text" id="contact-jid">
+			<label>Name:</label><input type="text" id="contact-name">
+		</div>
+		
+		<script type="text/javascript">
+		$(document).ready(function () {
+		    Attacher.connection = new Strophe.Connection(
+		        “http://bosh.metajack.im:5280/xmpp-httpbind”);
+		    Attacher.connection.attach(
+		        Attacher.JID, Attacher.SID, Attacher.RID, null);
+		    $(‘#log’).append(“<div>Session attached!</div>”);
+		    Attacher.connection.sendIQ(
+		        $iq({to: Strophe.getDomainFromJid(Attacher.JID),
+		             type: “get”})
+		            .c(‘query’, {xmlns:
+		                         ‘http://jabber.org/protocol/disco#info’}),
+		        function () {
+		            $(‘#log’).append(“<div>Response received “ +
+		                             “from server!</div>”);
+		        });
+		});
+		</script>
+		
+		<div id='log'>
+	    </div>
+	</body>
+</html>
+
 <?php
 
 
