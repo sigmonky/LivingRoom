3a610cdb5d68f77697e167b887a3a54fce538032
Logic Diner <logicdiner@gmail.com>
2011-08-24 23:59:07 -0700

    facebook callback

--- southpark/cueca.php
+++ southpark/cueca.php
@@ -114,6 +114,24 @@ $sid = $connection->getSid();
 		<script src="libs/underscore.js"></script> -->
 		
 		<script type="text/javascript">
+		$(document).ready(function(){
+			alert('a')
+		    // Attacher.connection = new Strophe.Connection("http://bosh.metajack.im:5280/xmpp-httpbind");
+		    // Attacher.connection.attach(Attacher.JID, Attacher.SID, Attacher.RID, null);
+		    // $('#log').append("<div>Session attached!</div>");
+		    // Attacher.connection.sendIQ(
+		    //     $iq({to: Strophe.getDomainFromJid(Attacher.JID),
+		    //          type: "get"})
+		    //         .c('query', {xmlns:
+		    //                      'http://jabber.org/protocol/disco#info'}),
+		    //     function () {
+		    //         $('#log').append("<div>Response received " +
+		    //                          "from server!</div>");
+		    //     });
+		});
+		</script>
+		
+		<script type="text/javascript">
 			
 			// our global config object
 			// plugins use this if it exists
@@ -250,24 +268,7 @@ $sid = $connection->getSid();
 	             RID: '<?=$sid?>'
 	         };
 	    </script>
-		
-		<script type="text/javascript">
-		$(document).ready(function () {
-			alert('a')
-		    // Attacher.connection = new Strophe.Connection("http://bosh.metajack.im:5280/xmpp-httpbind");
-		    // Attacher.connection.attach(Attacher.JID, Attacher.SID, Attacher.RID, null);
-		    // $('#log').append("<div>Session attached!</div>");
-		    // Attacher.connection.sendIQ(
-		    //     $iq({to: Strophe.getDomainFromJid(Attacher.JID),
-		    //          type: "get"})
-		    //         .c('query', {xmlns:
-		    //                      'http://jabber.org/protocol/disco#info'}),
-		    //     function () {
-		    //         $('#log').append("<div>Response received " +
-		    //                          "from server!</div>");
-		    //     });
-		});
-		</script>
+
 
 	</body>
 </html>
