5237d8f1707d288c77251ae4758e9f5bf0836947
Isaac Silva <isaacsou@gmail.com>
2011-08-25 17:49:56 -0400

    facebook php

--- southpark/cueca.php
+++ southpark/cueca.php
@@ -19,7 +19,7 @@ rtrim($fields_string,'&');
 $ch = curl_init();
 
 //set the url, number of POST vars, POST data 
-curl_setopt($ch,CURLOPT_URL,$url););
+curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 // curl_setopt($ch,CURLOPT_POST,count($fields));
 // curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
