2f9671860d1e6c1006d746bc8b77b81263b471a0
Logic Diner <logicdiner@gmail.com>
2011-08-24 23:58:27 -0700

    facebook callback

--- southpark/cueca.php
+++ southpark/cueca.php
@@ -254,20 +254,18 @@ $sid = $connection->getSid();
 		<script type="text/javascript">
 		$(document).ready(function () {
 			alert('a')
-		    Attacher.connection = new Strophe.Connection(
-		        "http://bosh.metajack.im:5280/xmpp-httpbind");
-		    Attacher.connection.attach(
-		        Attacher.JID, Attacher.SID, Attacher.RID, null);
-		    $('#log').append("<div>Session attached!</div>");
-		    Attacher.connection.sendIQ(
-		        $iq({to: Strophe.getDomainFromJid(Attacher.JID),
-		             type: "get"})
-		            .c('query', {xmlns:
-		                         'http://jabber.org/protocol/disco#info'}),
-		        function () {
-		            $('#log').append("<div>Response received " +
-		                             "from server!</div>");
-		        });
+		    // Attacher.connection = new Strophe.Connection("http://bosh.metajack.im:5280/xmpp-httpbind");
+		    // Attacher.connection.attach(Attacher.JID, Attacher.SID, Attacher.RID, null);
+		    // $('#log').append("<div>Session attached!</div>");
+		    // Attacher.connection.sendIQ(
+		    //     $iq({to: Strophe.getDomainFromJid(Attacher.JID),
+		    //          type: "get"})
+		    //         .c('query', {xmlns:
+		    //                      'http://jabber.org/protocol/disco#info'}),
+		    //     function () {
+		    //         $('#log').append("<div>Response received " +
+		    //                          "from server!</div>");
+		    //     });
 		});
 		</script>
 
