cb3d989e4f13ce3d8f72296b69b774c876317e94
Logic Diner <logicdiner@gmail.com>
2011-08-26 00:54:34 -0700

    php prebind

--- southpark/cueca.php
+++ southpark/cueca.php
@@ -24,7 +24,7 @@ class User {
 		if ($token != null){
 			$this->getFBUser();
 		}else{
-			$this->generateAnonymousSessionAttachment();
+		//	$this->generateAnonymousSessionAttachment();
 		}
         register_shutdown_function(array($this, 'shutdown'));
     }
