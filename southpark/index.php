610419a2a92e5c219ce67172196df7f08144151e
Logic Diner <logicdiner@gmail.com>
2011-08-28 15:48:00 -0700

    friends tab

--- southpark/index.php
+++ southpark/index.php
@@ -190,7 +190,7 @@ if ($facebook_user_profile['id'] != "") {
 				<div id='roster-area'>
 					 <ul class="friends-list"></ul>
 					<div class="invite-friends-btn">
-						<a href="#" onclick="FacebookNewInvite(); return false;">Invite Friends</a>
+						<a href="#" onclick="FacebookNewInvite(); return false;">Invite Friend To Chat</a>
 					</div>
 				</div>
 				
