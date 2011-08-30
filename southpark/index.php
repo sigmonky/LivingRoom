94f64f62bfd48b0ac1cf717ed259e60d08a476d1
Isaac Silva <isaacsou@gmail.com>
2011-08-30 16:40:53 -0400

    cosmetic changes

--- southpark/index.php
+++ southpark/index.php
@@ -293,7 +293,7 @@ if ($facebook_user_profile['id'] != "") {
 	</div>
 	<script>
 	
-	var isLoggedIn = true;
+	var isLoggedIn = false;
 	
 	var Attacher = {
 		JID: '<?=$user->sessionInfo['jid']?>',
