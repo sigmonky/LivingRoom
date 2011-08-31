45a107a9fada6b78fe76e6a9b371fd5861c86cb0
Isaac Silva <isaacsou@gmail.com>
2011-08-31 17:10:15 -0400

    change strophe lib

--- southpark/application/components/jabberclient.js
+++ southpark/application/components/jabberclient.js
@@ -99,7 +99,7 @@ _.extend(Jabber.Xmpp.prototype, Jabber.JsmvcCallback, Backbone.Events, {
 				log('RECV: ' + data);
 			};
 		
-			connection.conn.rawOutput = function (data) {
+			this.connection.conn.rawOutput = function (data) {
 				log('SENT: ' + data);
 			};
 		
