b0968f66b9d5100b7d094a7b626639749b07829f
Isaac Silva <isaacsou@gmail.com>
2011-08-31 16:52:06 -0400

    change strophe lib

--- southpark/application/controllers/MainController.js
+++ southpark/application/controllers/MainController.js
@@ -36,7 +36,7 @@ var App =  Backbone.Controller.extend({
 		this.headerView = new HeaderView({view: this});
 		this.paneView = new PaneView();
 
-		window.chat = new JabberClient.Xmpp();
+		window.chat = new JabberClient.Xmpp({});
 
       //  this.view = new ChatView({model: this.model, remoteJid: remoteJid, el: $('#all_fans_view'), name: name});
 
