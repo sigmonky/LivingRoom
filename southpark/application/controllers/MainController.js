c7004f194536bdeadb8431fc03137a6b3057173e
Isaac Silva <isaacsou@gmail.com>
2011-08-31 15:38:05 -0400

    change strophe lib

--- southpark/application/controllers/MainController.js
+++ southpark/application/controllers/MainController.js
@@ -1,7 +1,7 @@
 var App =  Backbone.Controller.extend({	
 	paneView: null,
 	headerView: null,
-	connection: null,
+	
 	routes: {
         "":       "index",
         "allfans":  "index",
@@ -22,37 +22,34 @@ var App =  Backbone.Controller.extend({
 		
 		/*Start XMPP Connection */
 
-	//	this.connection = null;
+		var connection = null;
 	 	var startTime = null;
 	 	var BOSH_SERVICE = '/http-bind';
 
-		this.connection = new Strophe.Connection(BOSH_SERVICE);
+		connection = new Strophe.Connection(BOSH_SERVICE);
 		
 		// Strophe.log = function (lvl, msg) { log(msg); };
-		this.connection.attach(Attacher.JID, Attacher.SID, Attacher.RID, onConnect);
+		connection.attach(Attacher.JID, Attacher.SID, Attacher.RID, onConnect);
 		
 		
 			    // set up handler
-		this.connection.addHandler(onResult, null, 'iq',	'result', 'disco-1', null);
+		connection.addHandler(onResult, null, 'iq',	'result', 'disco-1', null);
 		
 		
-		this.connection.rawInput = function (data) {
+		connection.rawInput = function (data) {
 				log('RECV: ' + data);
 			};
 		
-			this.connection.rawOutput = function (data) {
+			connection.rawOutput = function (data) {
 				log('SENT: ' + data);
 			};
 		
 		// send disco#info to jabber.org
 		var iq = $iq({to: 'jabber.org',	type: 'get',id: 'disco-1'}).c('query', {xmlns: Strophe.NS.DISCO_INFO}).tree()
 		
-		this.connection.send(iq);
+		connection.send(iq);
 		
-		//connection.addHandler(this.onMessage, null, 'message', null, null,  null); 
-		this.connection.addHandler(this.onMessage, null, "message", "chat");
-	
-	//	JabberClient.init(connection);
+		JabberClient.init(connection);
 		
 		/* All Fans View Start up */
         this.model = new models.ChatRoomModel();
@@ -63,8 +60,6 @@ var App =  Backbone.Controller.extend({
 		this.headerView = new HeaderView({view: this});
 		this.paneView = new PaneView();
 		
-		this.joinRoom(RoomJid);
-		
       //  this.view = new ChatView({model: this.model, remoteJid: remoteJid, el: $('#all_fans_view'), name: name});
 
 
@@ -73,26 +68,12 @@ var App =  Backbone.Controller.extend({
         return this;
     }, 
 
-	joinRoom: function(roomJid){
-			var roomJid = roomJid;
-			var nickname = 'guest_'+Math.floor(Math.random()*1111001);
-			this.connection.muc.join(roomJid, nickname, this.onMessage, this.onPresence);
-	},
-
 	index: function(){
 		this.paneView.renderAllFans();
 	},
 
 	goToMyFriends: function() {
 		this.paneView.renderMyFriends();
-	},
-	
-	onMessage: function(m){
-		console.log('on message');
-	},
-	
-	onPresence: function(m){
-		console.log('on onPresence');
 	},
 	
 	goToBuzz: function() {
