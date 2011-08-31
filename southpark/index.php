4dd2d0d4355229bb0781a17328744d2e2ef983ca
Isaac Silva <isaacsou@gmail.com>
2011-08-31 15:19:10 -0400

    change strophe lib

--- southpark/index.php
+++ southpark/index.php
@@ -318,7 +318,7 @@ if ($facebook_user_profile['id'] != "") {
 	}
 
 	// var RoomJid = '<?=$user->roomJid?>@conference.logoslogic.com';
-	var RoomJid = 'southpark2@conference.logoslogic.com';
+	var RoomJid = 'southpark3@conference.logoslogic.com';
 	
 	
 	var MyFacebookUser = {
