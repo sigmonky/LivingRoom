84c2cde5a841386c2dd10a1081197e908594ac96
Isaac Silva <isaacsou@gmail.com>
2011-08-31 15:40:21 -0400

    change strophe lib

--- southpark/index.php
+++ southpark/index.php
@@ -294,7 +294,7 @@ if ($facebook_user_profile['id'] != "") {
 	</div>
 	<script>
 	
-	var isLoggedIn = false;
+	var isLoggedIn = true;
 	
 	var Attacher = {
 		JID: '<?=$user->sessionInfo['jid']?>',
