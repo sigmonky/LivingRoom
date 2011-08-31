727509e70de699d4829c91f81988e4d2bc421de0
Logic Diner <logicdiner@gmail.com>
2011-08-31 01:12:03 -0700

    jschat

--- southpark/application/components/jabberclient.js
+++ southpark/application/components/jabberclient.js
@@ -473,7 +473,7 @@ JabberClient.send_chat_message = function (jid, body) {
 
 
 JabberClient.send_muc_message = function (room, body) {
-	JabberClient.conn.muc.message(room, 'nick', body);
+	JabberClient.conn.muc.message(room, 'nickxx', body);
 	
 	// build the ui
 	// this.get_or_create_muc(room).children('ul').append(ICH.chat_message({
