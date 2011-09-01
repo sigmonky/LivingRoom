e869b7230552364dc32dd5e7351f1a3a8ade1c9c
Isaac Silva <isaacsou@gmail.com>
2011-09-01 12:28:41 -0400

    change strophe lib

--- southpark/application/components/jabberclient.js
+++ southpark/application/components/jabberclient.js
@@ -337,11 +337,11 @@ _.extend(Jschat.Xmpp.prototype, Jschat.JsmvcCallback, Backbone.Events, {
 	initialize: function(){
 		this.connection = new Strophe.Connection(this.options.bosh_service);
 		this.roster = new Jschat.Roster();
-		this.chatlog = new Jschat.ChatLog();
-		this.view = new Jschat.ChatView({
-			el: $('#'+this.options.view_el_id)
-		});
-		this._welcomeSent = false;
+		// this.chatlog = new Jschat.ChatLog();
+		// this.view = new Jschat.ChatView({
+		// 	el: $('#'+this.options.view_el_id)
+		// });
+//		this._welcomeSent = false;
 //	    this.connection.rawInput = function (data) { console.log('RECV: ' + data); };
 //	    this.connection.rawOutput = function (data) { console.log('SEND: ' + data); };
 //		listen events
