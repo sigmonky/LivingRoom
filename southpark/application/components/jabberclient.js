7871d2e4fa8c70215bdc3269a6d10d5e54caf705
Logic Diner <logicdiner@gmail.com>
2011-09-03 21:25:42 -0700

    setvcard

--- southpark/application/components/jabberclient.js
+++ southpark/application/components/jabberclient.js
@@ -264,7 +264,13 @@ _.extend(Jabber.Xmpp.prototype, Jabber.JsmvcCallback, Backbone.Events, {
 		//console.log('pos '+pos);
 		var full_nickname = from.substring(pos,from.length);
 		var user_nick  = full_nickname.split('_')[0];
-		user_nick = stripslashes(user_nick);
+		
+		var  posn = user_nick.indexOf("\20");
+		while (posn > -1){ 
+		   user_nick = user_nick.substring(0,posn) + " " + user_nick.substring(posn+3); 
+	       posn = user_nick.indexOf("\20");
+	     }; 
+	 
 		user_nick  = user_nick.replace('20','');
 		
 		var facebook_id  = full_nickname.split('_')[1];
