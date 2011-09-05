6d960b21eb1119cbacbd2ff0480fb61ea6da6db3
Logic Diner <logicdiner@gmail.com>
2011-09-05 01:37:17 -0700

    tweet msg

--- southpark/fbmain.php
+++ southpark/fbmain.php
@@ -75,15 +75,15 @@
     }
 
 	/* Login / Logout Issue */
-	if (isset($_SERVER['HTTP_COOKIE'])) {
-	    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-	    foreach($cookies as $cookie) {
-	        $parts = explode('=', $cookie);
-	        $name = trim($parts[0]);
-	        setcookie($name, '', time()-1000);
-	        setcookie($name, '', time()-1000, '/');
-	    }
-	}
+	// if (isset($_SERVER['HTTP_COOKIE'])) {
+	//     $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+	//     foreach($cookies as $cookie) {
+	//         $parts = explode('=', $cookie);
+	//         $name = trim($parts[0]);
+	//         setcookie($name, '', time()-1000);
+	//         setcookie($name, '', time()-1000, '/');
+	//     }
+	// }
     
     function d($d){
         echo '<pre>';
