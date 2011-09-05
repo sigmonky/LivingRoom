20e7fadc132de35f8b44b0ac32f190d8b85f3b3d
Logic Diner <logicdiner@gmail.com>
2011-09-05 01:13:31 -0700

    tweet msg

--- southpark/twitter/index.php
+++ southpark/twitter/index.php
@@ -20,10 +20,15 @@ $oauth_token = $_GET['oauth_token'];
 		$twitterObj->setToken($_GET['oauth_token']);
 		$token = $twitterObj->getAccessToken();
 		$twitterObj->setToken($token->oauth_token, $token->oauth_token_secret);	  	
-		$_SESSION['ot'] = $token->oauth_token;
-		$_SESSION['ots'] = $token->oauth_token_secret;
-		echo $_SESSION['ot'];
-		 echo $_SESSION['ots'];
+		if(isset($_SESSION['ot'])){
+			echo $_SESSION['ot'];
+		}else{
+			$_SESSION['ot'] = $token->oauth_token;
+			$_SESSION['ots'] = $token->oauth_token_secret;
+			echo $_SESSION['ot'];
+			 echo $_SESSION['ots'];
+		}
+
 		
 		//header('Location:http://www.logoslogic.com/chat/LivingRoom/southpark/index.php#buzz');
 		// echo "<pre>200 OK</pre>";
