6828fff9f046674047e99fb9f8be84bf54b65556
Logic Diner <logicdiner@gmail.com>
2011-09-05 00:42:06 -0700

    tweet msg

--- southpark/twitter/post.php
+++ southpark/twitter/post.php
@@ -7,27 +7,29 @@ include 'lib/EpiOAuth.php';
 include 'lib/EpiTwitter.php';
 include 'lib/secret.php';
 
-	if(isset($_SESSION['ot'])){
-		$twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
-		$msg = $_REQUEST['tweet'];
-		$twitterObj->setToken($_SESSION['ot'], $_SESSION['ots']);
-		//echo '1 - '.$_SESSION['ot'];
-		//echo '2 -'.$_SESSION['ots'];
-		$twitterInfo= $twitterObj->get_accountVerify_credentials();
-		$twitterInfo->response;
-		//$username = $twitterInfo->screen_name;
-		$update_status = $twitterObj->post_statusesUpdate(array('status' => $msg));
-		$temp = $update_status->response;
-		// header('Cache-Control: no-cache, must-revalidate');
-		// 	  	header('Expires: Mon, 26 Jul 2222 05:00:00 GMT');
-	  	header('Content-type: application/json');
-		echo "{\"status\": \"posted\"}";
-
-	}
-	else{
-		// header('Cache-Control: no-cache, must-revalidate');
-		// header('Expires: Mon, 26 Jul 2222 05:00:00 GMT');
-	  	header('Content-type: application/json');
-		echo "{\"status\": \"not_connected\"}";
-	}
+	echo '1 - '.$_SESSION['ot'];
+	echo '2 -'.$_SESSION['ots'];
+	// if(isset($_SESSION['ot'])){
+	// 	$twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
+	// 	$msg = $_REQUEST['tweet'];
+	// 	$twitterObj->setToken($_SESSION['ot'], $_SESSION['ots']);
+	// 	//echo '1 - '.$_SESSION['ot'];
+	// 	//echo '2 -'.$_SESSION['ots'];
+	// 	$twitterInfo= $twitterObj->get_accountVerify_credentials();
+	// 	$twitterInfo->response;
+	// 	//$username = $twitterInfo->screen_name;
+	// 	$update_status = $twitterObj->post_statusesUpdate(array('status' => $msg));
+	// 	$temp = $update_status->response;
+	// 	// header('Cache-Control: no-cache, must-revalidate');
+	// 	// 	  	header('Expires: Mon, 26 Jul 2222 05:00:00 GMT');
+	//   	header('Content-type: application/json');
+	// 	echo "{\"status\": \"posted\"}";
+	// 
+	// }
+	// else{
+	// 	// header('Cache-Control: no-cache, must-revalidate');
+	// 	// header('Expires: Mon, 26 Jul 2222 05:00:00 GMT');
+	//   	header('Content-type: application/json');
+	// 	echo "{\"status\": \"not_connected\"}";
+	// }
 ?> 
