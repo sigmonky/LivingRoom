6498502952ef4d2b8dce6f2b934906c7838a1bc0
Isaac Silva <isaacsou@gmail.com>
2011-09-06 16:13:18 -0400

    one to one

--- southpark/application/views/PaneView.js
+++ southpark/application/views/PaneView.js
@@ -68,31 +68,31 @@ var PaneView = Backbone.View.extend({
 		
 		
 		/* Buzz View Begin */
-		var twitter_message_panel = $("#twitter_message_panel");
-		var mainBuzzTweetView = new BuzzTweetInputView();
-		$(mainBuzzTweetView.render().el).prependTo(twitter_message_panel);
-		
-		$("input#input_tweet").keyup(function(){
-			console.log('keyup')
-			var box=$(this).val();
-			var main = box.length *100;
-			var value= (main / 140);
-			var count= 140 - box.length;
-
-			if(box.length <= 140){
-				$('#tweet_count').html(count);
-			}
-			else
-			{
-				alert('Character Limit Exceeded!');
-				return false;
-			}
-		});
-		
-		
-		var buzz_main_panel = $("#buzz_main_panel");
-		var mainBuzzView = new BuzzMainView(this.model);
-		$(mainBuzzView.render().el).prependTo(buzz_main_panel);
+		// var twitter_message_panel = $("#twitter_message_panel");
+		// var mainBuzzTweetView = new BuzzTweetInputView();
+		// $(mainBuzzTweetView.render().el).prependTo(twitter_message_panel);
+		// 
+		// $("input#input_tweet").keyup(function(){
+		// 	console.log('keyup')
+		// 	var box=$(this).val();
+		// 	var main = box.length *100;
+		// 	var value= (main / 140);
+		// 	var count= 140 - box.length;
+		// 
+		// 	if(box.length <= 140){
+		// 		$('#tweet_count').html(count);
+		// 	}
+		// 	else
+		// 	{
+		// 		alert('Character Limit Exceeded!');
+		// 		return false;
+		// 	}
+		// });
+		// 
+		// 
+		// var buzz_main_panel = $("#buzz_main_panel");
+		// var mainBuzzView = new BuzzMainView(this.model);
+		// $(mainBuzzView.render().el).prependTo(buzz_main_panel);
 	// 	
 	// 	var url = 'http://tweetriver.com/camilarc/south-park-test.json?';
 	// 	
